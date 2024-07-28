d287a369ad95805314ef0a3fe33616aa0bfc7d08
Baihaqi Muhammad <baihaqi.m551@gmail.com>
2024-07-29 01:19:16 +0700

    [UPDATE] update filtering product

--- src/main/java/com/muffincrunchy/oeuvreapi/model/constant/ItemType.java
+++ src/main/java/com/muffincrunchy/oeuvreapi/model/constant/ItemType.java
@@ -4,23 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum ItemType {
-    DIGITAL("Digital"),
-    PHYSICAL("Physical");
-
-    public final String label;
-    private ItemType(String label) {
-        this.label = label;
-    }
-
-    private static final Map<String, ItemType> map;
-    static {
-        map = new HashMap<String, ItemType>();
-        for (ItemType itemType : ItemType.values()) {
-            map.put(itemType.label, itemType);
-        }
-    }
-
-    public static ItemType getByLabel(String label) {
-        return map.get(label);
-    }
+    DIGITAL,
+    PHYSICAL
 }
